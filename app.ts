07b0d527bc599587e23b090282810e14fa393012
piinniitta <panita3824@gmail.com>
2023-08-28 19:33:09 +0700

    Create app.ts

---
+++ app.ts
@@ -0,0 +1,6 @@
+import { merge } from './merge';
+
+const collection1 = [1, 3, 5, 7, 9];
+const collection2 = [2, 4, 6, 8, 10];
+const result = merge(collection1, collection2);
+console.log(result);
